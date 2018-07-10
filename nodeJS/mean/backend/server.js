b055cf54fa8201313b06bb648dfb32396e7e78bb
Habib Farahani <habibf@hotmail.com>
2018-07-10 17:23:37 -0500

    econd update: Backend mostly

--- nodeJS/mean/backend/server.js
+++ nodeJS/mean/backend/server.js
@@ -1,10 +1,63 @@
 //import express from 'express';
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Issue = require('./models/issue');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect('');
+const connection = mongoose.coonection;
+
+connection.once('open', () => {
+    console.log('MongoDB connected successfully');
+});
+
+router.route('/issue').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issues);
+        }
+    })
+});
+
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issue);
+        }
+    });
+
+});
+
+
+router.route('/issues/add').post((req, res) => {
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.staus(200).json({ 'issue': 'Added Successfully' });
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create  anew record');
+        })
 })
+
+app.use('/', router);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!!');
+// })
 
 app.listen(4000, () => console.log('Express Server runnig on port 4000'));
